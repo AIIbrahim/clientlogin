e3a556d117a24b37fc2bf54f62d32544cea464d5
AZEEZ <ibazeez1@gmail.com>
2020-01-10 12:42:52 +0100

    almost done

--- include/footer.php
+++ include/footer.php
@@ -7,10 +7,10 @@
 
 <!-- Bootstrap JS -->
 
-
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
-	<script type="text/javascript" src="js/jquery.ripples-min.js"></script>
-	<script type="text/javascript" src="js/typed.min.js"></script>
+        <!-- jQuery -->
+        <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+        
+        <!-- Bootstrap JS -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
 </html>
--- include/header.php
+++ include/header.php
@@ -3,39 +3,59 @@
 <html>
 
     <head>
-    <meta charset="UTF-8" />
-	<meta name="viewport" content="Width=device-width, initial-scale=1.0" />
-	<!--css links-->
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- js scripts -->
-	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/respond.js"></script>
-    
+        <title>Client Address Book</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
 
 </head>
     
     <body style="padding-top: 60px;"> 
-    <header>
-    <nav class="pull-right right" role="navigation">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynav">
-				<i class="fa fa-bars"></i>
-			</button>				
-			<div class="collapse" id="mynav">
-				<ul class="nav navbar-nav">
-					<li><a href="#">Home</a></li>
-								
-					<li><a href="#">About Us</a></li>
-										
-					<li><a href="#">Service</a></li>
-										
-					<li ><a href="#">Contact Us</a></li>
-				</ul>
-			</div>
-		</nav>		    
-        </header>
+    						
+	<header >
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+
+<div class="container-fluid">
+
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="clients.php">CLIENT<strong>MANAGER</strong></a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-collapse">
+        
+       
+        <ul class="nav navbar-nav">
+            <li><a href="clients.php">My Clients</a></li>
+            <li><a href="add.php">Add Client</a></li>
+        </ul>
+
+        <ul class="nav navbar-nav navbar-right">
+            <p class="navbar-text">Welcome, Azeez!</p>
+
+            <li><a href="logout.php">Log out</a></li>
+        </ul>
+       
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Log in</a></li>
+        </ul>
+       
+    </div>
+
+</div>
+
+</nav>					
+	</header>
     <!--
                   
     <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
